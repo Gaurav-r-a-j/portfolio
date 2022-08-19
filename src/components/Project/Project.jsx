f651baecb895a189ff0edab55de451dd6536a356
sstarx <gk5271@srmist.edu.in>
2022-08-19 16:59:49 +0530

    updated project secion make an array of objects for project section for code readibility

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { project3, netflixClone, portfolio1, portfolio2, sstarx, chatapp, chemvigyan, teachingApp, textutils, tier3wale, spotifyHomepage, myntraClone, facebookClone, spotify, notesapp, idicuss, postmaster } from '../../images';
+// import { project3, netflixClone, portfolio1, portfolio2, sstarx, chatapp, chemvigyan, teachingApp, textutils, tier3wale, spotifyHomepage, myntraClone, facebookClone, spotify, notesapp, idicuss, postmaster } from '../../images';
+
+import projectData from '../../projectData';
 
 // const TechUsed = ['HTML',"CSS",'REACT','JAVASCRIPT','MONGODB','MONGOOSE']
 
@@ -248,17 +250,29 @@ const Heading = styled.h1`
 
 const Project = ({ theme }) => {
 
-  const [showMore, setShowMore] = useState(3);
+  const [showMore, setShowMore] = useState(13);
 
   const LoadMore = () => {
-    console.log("loaded")
-    console.log(showMore)
-    setShowMore(showMore + 4);
+    // console.log("loaded")
+    // console.log(showMore)
+    setShowMore(showMore - 4);
   }
 
   const Showless = () => {
-    setShowMore(4);
+    setShowMore(13);
   }
+
+  const output = projectData.map((item)=>{
+    // console.log(item);
+    return item;
+  }).filter((x)=> x.projectNumber > showMore)
+
+
+  console.log(output[0].projectNumber)
+
+  // output.map((item)=>{
+  //   console.log(item)
+  // })
 
 
 
@@ -266,223 +280,29 @@ const Project = ({ theme }) => {
     <div style={{ position: "relative" }}>
       <Heading>Projects</Heading>
 
-      {showMore > 0 && <ProjectCard
-        number="17"
-        theme={theme}
-        image={netflixClone}
-        title="Netflix Clone (react)"
-        text="This is a React application i made it recently in this i have also use TMDB movies api for showing movies poster"
-        TechUsed={['React', 'CSS', 'Vanilla-Tilt', 'Styled-Components','TMDB API']}
-        siteLink="https://netflixx-react.netlify.app/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/Netflix-clone"
-      />}
+      {
+         output.map((item, index) => 
+          // console.log(item)
+           <ProjectCard
+            key={index}
+            number={item.projectNumber}
+            theme={theme}
+            image={item.imgUrl}
+            title={item.title}
+            text={item.text}
+            TechUsed={item.techUsed}
+            liveLink={item.liveLink}
+            codeLink={item.codeLink}
+          />
 
-      {showMore > 0 && <ProjectCard
-        number="16"
-        theme={theme}
-        image={project3}
-        title="Full Stack Portfolio"
-        text="This was my first big project in which i created my own api and also have authentication functionality."
-        TechUsed={['HTML', 'CSS', "REACT", "MONGODB", 'EXPRESS', "MAILTRAP","CLOUDINARY",'JWT']}
-        reverse="row-reverse"
-        siteLink="https://gaurav-raj-portfolio.herokuapp.com/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/New-Mern-Stack-portfolio"
-
-      />}
-
-
-      {showMore > 0 && <ProjectCard
-        number="15"
-        theme={theme}
-        image={spotifyHomepage}
-        title="Spotfiy Homepage"
-        text="This is a simple landing page of spotify Website that i make just to practice my skills when I learned basic of html and css"
-        TechUsed={['HTML', 'CSS']}
-        siteLink="https://gaurav-r-a-j.github.io/spotify-homepage/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/spotify-homepage"
-      />}
-
-      {showMore > 0 && <ProjectCard
-        number="14"
-        theme={theme}
-        image={teachingApp}
-        title="Teaching"
-        text="This is my first Website that i make usig html and css, I named it as sstarx"
-        TechUsed={['HTML', 'CSS', 'JAVASCRIPT']}
-        reverse="row-reverse"
-        siteLink="https://gaurav-r-a-j.github.io/Teaching-website/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/Teaching-website"
-      />
+        )
       }
-
-      {showMore > 4 && <ProjectCard
-        number="13"
-        theme={theme}
-        image={facebookClone}
-        title="Facebook Clone"
-        text="This is just a simple facebook clone I made this in the beginning of my journey by watching a tutorial but i got to learn many things after this tutorial like how we can make a reusable class reusable items and many more"
-        TechUsed={['HTML', 'CSS', 'JAVASCRIPT']}
-        siteLink="https://gaurav-r-a-j.github.io/Facebook-clone/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/Facebook-clone"
-      />
-      }
-
-      {showMore > 4 && <ProjectCard
-        number="12"
-        theme={theme}
-        image={portfolio2}
-        title="Portfolio Website"
-        text="This is a simple one page portfolio website Website that i make usig html and css and a little bit of javascript for dark mode functinality"
-        TechUsed={['HTML', 'CSS', 'JAVASCRIPT']}
-        reverse="row-reverse"
-        siteLink="https://gaurav-r-a-j.github.io/portfolio1/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/portfolio1"
-
-      />}
-
-      {showMore > 4 && <ProjectCard
-        number="11"
-        theme={theme}
-        image={notesapp}
-        title="Notes App"
-        text="This is another simple website where use can add some note  or say a simple To do list app"
-        TechUsed={['HTML', 'CSS', 'JAVASCRIPT']}
-        siteLink="https://gaurav-r-a-j.github.io/Notes-app/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/Notes-app"
-      />}
-
-      {showMore > 4 && <ProjectCard
-        number="10"
-        theme={theme}
-        image={chemvigyan}
-        title="Chemvigyan"
-        text="This is my first website that i made for some client at that time I dont have any backend know ledge but I have learnde Wordpress before so i made it using wordpress"
-        TechUsed={['Wordpress']}
-        reverse="row-reverse"
-        siteLink="https://chemvigyan.com/"
-        codeLink="https://chemvigyan.com/"
-      />}
-
-      {showMore > 8 && <ProjectCard
-        number="09"
-        theme={theme}
-        image={tier3wale}
-        title="Tier3wale"
-        text="This is another wordpress website that i made it for our youtube channel in starting it's also on wordpress as at time my semester was coming and i don't have enough time so i choose wordpress"
-        TechUsed={['Wordpress']}
-        siteLink="http://tier3wale.me/"
-        codeLink="http://tier3wale.me/"
-
-      />}
-
-      {showMore > 8 && <ProjectCard
-        number="08"
-        theme={theme}
-        image={myntraClone}
-        title="Myntra Homepage"
-        text="This is a simple myntra clone that i made during my learning to test my learning progress"
-        TechUsed={['HTML', "CSS"]}
-        reverse="row-reverse"
-        siteLink="https://gaurav-r-a-j.github.io/Myntra-clone/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/Myntra-clone"
-
-      />}
-
-      {showMore > 8 && <ProjectCard
-        number="07"
-        theme={theme}
-        image={chatapp}
-        title="Chat App"
-        text="This is my first messaging application that i learned when i was learning php language i learnd my things from it. Due to some hosting issues i am unable to uplaod it backend it's only frontend here"
-        TechUsed={['HTML', "CSS", 'JAVASCRIPT']}
-        siteLink="https://gaurav-r-a-j.github.io/Chat-app/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/Chat-app"
-
-      />}
-
-
-      {showMore > 8 && <ProjectCard
-        number="06"
-        theme={theme}
-        image={spotify}
-        title="Spotfiy Music"
-        text="This is a simple music application app of say a spotify clone that i made during my learning process to enchance my javascript concepts"
-        TechUsed={['HTML', "CSS", 'JAVASCRIPT']}
-        reverse="row-reverse"
-        siteLink="https://gaurav-r-a-j.github.io/Spotify-clone/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/Spotify-clone"
-
-      />}
-
-
-      {showMore > 12 && <ProjectCard
-        number="05"
-        theme={theme}
-        image={textutils}
-        title="Textutils"
-        text="This is my React app that i made when i started learning React."
-        TechUsed={['REACT', 'BOOTSTRAP', 'JAVASCRIPT']}
-        siteLink="https://my-textutils1.netlify.app/"
-        codeLink=""
-      />}
-
-
-      {showMore > 12 && <ProjectCard
-        number="04"
-        theme={theme}
-        image={postmaster}
-        title="Textutils"
-        text="This is s simple application to send get and post request, I made it to inhance my fetch api concept at that time."
-        TechUsed={['BOOTSTRAP', 'JAVASCRIPT', 'PRETTIER']}
-        reverse="row-reverse"
-        siteLink="https://gaurav-r-a-j.github.io/Api-request-app/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/Api-request-app"
-
-      />}
-
-      {showMore > 12 && <ProjectCard
-        number="03"
-        theme={theme}
-        image={idicuss}
-        title="iDiscuss"
-        text="This is my first fully backend php website in which i have added login functionality , search functionality and many thing I learnd it from Codewithharry yt channel"
-        TechUsed={['BOOTSTRAP', 'PHP', 'MYSQL']}
-        siteLink="http://idiscuss.lovestoblog.com/index.php"
-        codeLink="http://idiscuss.lovestoblog.com/index.php"
-
-      />}
-
-      {showMore > 12 && <ProjectCard
-        number="02"
-        theme={theme}
-        image={portfolio1}
-        title="My first Portfolio"
-        text="I created this from a tutorial available on youtube"
-        TechUsed={['HTML', 'CSS']}
-        reverse="row-reverse"
-        siteLink="https://sstarx.in/portfolio.html"
-        codeLink="https://github1s.com/Gaurav-r-a-j/sstarx.github.io/blob/main/portfolio.html"
-
-
-      />}
-
-      {showMore > 14 && <ProjectCard
-        number="01"
-        theme={theme}
-        image={sstarx}
-        title="SSTARX "
-        text="My first Website after learning html and css and at that time I can proudly say that i have made this website with my own without watching any tutorial or else and it's my first website from where my journey starts so it's always be a special for me I named it sstarx."
-        TechUsed={['HTML', 'CSS']}
-        siteLink="https://sstarx.in/"
-        codeLink="https://github1s.com/Gaurav-r-a-j/sstarx.github.io"
-      />}
-
 
 
 
       <Show style={{ background: `${theme}` }}>
-        <ShorMoreButton onClick={() => LoadMore()}>Load More Projects</ShorMoreButton>
-        {showMore > 3 && <ShorMoreButton onClick={() => Showless()}>Show less</ShorMoreButton>}
+        {showMore > 0 && <ShorMoreButton onClick={() => LoadMore()}>Load More Projects</ShorMoreButton>}
+        {showMore <13 && <ShorMoreButton onClick={() => Showless()}>Show less</ShorMoreButton>}
       </Show>
     </div>
   )
@@ -490,10 +310,8 @@ const Project = ({ theme }) => {
 
 
 
-export const ProjectCard = ({ theme, number, reverse, image, title, text, TechUsed, codeLink, siteLink }) => {
+export const ProjectCard = ({ theme, number, reverse, image, title, text, TechUsed, codeLink, liveLink }) => {
   const black = { background: `${theme}` };
-  // TechUsed = new Array(TechUsed);
-  // console.log(TechUsed)
   const Tech = TechUsed;
   // console.log(TechUsed)
   return (
@@ -516,7 +334,7 @@ export const ProjectCard = ({ theme, number, reverse, image, title, text, TechUs
 
         </Technologies>
         <ViewButtonWrapper>
-          <a href={siteLink} target="blank" title='visit site'><ViewButton>View Live</ViewButton></a>
+          <a href={liveLink} target="blank" title='visit site'><ViewButton>View Live</ViewButton></a>
           <a href={codeLink} target="blank" title='view code'><ViewButton>View Code</ViewButton></a>
         </ViewButtonWrapper>
       </AboutProject>
---
+++ src/projectData.js
@@ -0,0 +1,202 @@
+import { project3, netflixClone, portfolio1, portfolio2, sstarx, chatapp, chemvigyan, teachingApp, textutils, tier3wale, spotifyHomepage, myntraClone, facebookClone, spotify, notesapp, idicuss, postmaster }
+from './images/index';
+
+const projectData = [
+
+    {
+        projectNumber: "17",
+        imgUrl: netflixClone ,
+        title: "Netflix Clone (react)",
+        text: "This is a React application i made it recently in this i have also use TMDB movies api for showing movies poster",
+        techUsed: ['React', 'CSS', 'Vanilla-Tilt', 'Styled-Components', 'TMDB API'],
+        liveLink: "https://netflixx-react.netlify.app/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/Netflix-clone",
+    },
+
+    {
+        projectNumber: "16",
+        imgUrl: project3,
+        title: "Full Stack Portfolio",
+        text: "This was my first big project in which i created my own api and also have authentication functionality.",
+        techUsed: ['HTML', 'CSS', "REACT", "MONGODB", 'EXPRESS', "MAILTRAP", "CLOUDINARY", 'JWT'],
+
+        liveLink: "https://gaurav-raj-portfolio.herokuapp.com/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/New-Mern-Stack-portfolio",
+
+    },
+
+
+    {
+        projectNumber: "15",
+        imgUrl: spotifyHomepage ,
+        title: "Spotfiy Homepage",
+        text: "This is a simple landing page of spotify Website that i make just to practice my skills when I learned basic of html and css",
+        techUsed: ['HTML', 'CSS'],
+        liveLink: "https://gaurav-r-a-j.github.io/spotify-homepage/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/spotify-homepage",
+    },
+
+    {
+        projectNumber: "14",
+        imgUrl: teachingApp,
+        title: "Teaching",
+        text: "This is my first Website that i make usig html and css, I named it as sstarx",
+        techUsed: ['HTML', 'CSS', 'JAVASCRIPT'],
+        liveLink: "https://gaurav-r-a-j.github.io/Teaching-website/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/Teaching-website",
+
+    },
+
+    {
+        projectNumber: "13",
+
+        imgUrl: facebookClone ,
+        title: "Facebook Clone",
+        text: "This is just a simple facebook clone I made this in the beginning of my journey by watching a tutorial but i got to learn many things after this tutorial like how we can make a reusable class reusable items and many more",
+        techUsed: ['HTML', 'CSS', 'JAVASCRIPT'],
+        liveLink: "https://gaurav-r-a-j.github.io/Facebook-clone/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/Facebook-clone",
+    },
+
+    {
+        projectNumber: "12",
+        imgUrl: portfolio2,
+        title: "Portfolio Website",
+        text: "This is a simple one page portfolio website Website that i make usig html and css and a little bit of javascript for dark mode functinality",
+        techUsed: ['HTML', 'CSS', 'JAVASCRIPT'],
+        liveLink: "https://gaurav-r-a-j.github.io/portfolio1/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/portfolio1",
+
+    },
+
+    {
+        projectNumber: "11",
+        imgUrl: notesapp ,
+        title: "Notes App",
+        text: "This is another simple website where use can add some note  or say a simple To do list app",
+        techUsed: ['HTML', 'CSS', 'JAVASCRIPT'],
+        liveLink: "https://gaurav-r-a-j.github.io/Notes-app/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/Notes-app",
+    },
+
+    {
+        projectNumber: "10",
+        imgUrl: chemvigyan ,
+        title: "Chemvigyan",
+        text: "This is my first website that i made for some client at that time I dont have any backend know ledge but I have learnde Wordpress before so i made it using wordpress",
+        techUsed: ['Wordpress'],
+        liveLink: "https://chemvigyan.com/",
+        codeLink: "https://chemvigyan.com/",
+    },
+
+    {
+        projectNumber: "09",
+        imgUrl:  tier3wale,
+        title: "Tier3wale",
+        text: "This is another wordpress website that i made it for our youtube channel in starting it's also on wordpress as at time my semester was coming and i don't have enough time so i choose wordpress",
+        techUsed: ['Wordpress'],
+        liveLink: "http://tier3wale.me/",
+        codeLink: "http://tier3wale.me/",
+
+    },
+
+    {
+        projectNumber: "08",
+        imgUrl:  myntraClone ,
+        title: "Myntra Homepage",
+        text: "This is a simple myntra clone that i made during my learning to test my learning progress",
+        techUsed: ['HTML', "CSS"],
+        liveLink: "https://gaurav-r-a-j.github.io/Myntra-clone/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/Myntra-clone",
+
+    },
+
+    {
+
+        projectNumber: "07",
+
+        imgUrl: chatapp ,
+        title: "Chat App",
+        text: "This is my first messaging application that i learned when i was learning php language i learnd  my things from it. Due to some hosting issues i am unable to uplaod it backend it's only frontend here",
+        techUsed: ['HTML', "CSS", 'JAVASCRIPT'],
+        liveLink: "https://gaurav-r-a-j.github.io/Chat-app/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/Chat-app",
+
+    },
+
+
+    {
+        projectNumber: "06",
+
+        imgUrl: spotify ,
+        title: "Spotfiy Music",
+        text: "This is a simple music application app of say a spotify clone that i made    during my learning process to enchance my javascript concepts",
+        techUsed: ['HTML', "CSS", 'JAVASCRIPT'],
+
+        liveLink: "https://gaurav-r-a-j.github.io/Spotify-clone/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/Spotify-clone",
+
+    },
+
+
+    {
+        projectNumber: "05",
+        imgUrl:  textutils ,
+        title: "Textutils",
+        text: "This is my React app that i made when i started learning React.",
+        techUsed: ['REACT', 'BOOTSTRAP', 'JAVASCRIPT'],
+        liveLink: "https://my-textutils1.netlify.app/",
+        codeLink: "",
+    },
+
+
+    {
+        projectNumber: "04",
+        imgUrl:  postmaster ,
+        title: "Textutils",
+        text: "This is s simple application to send get and post request, I made it to  inhance my fetch api concept at that time.",
+        techUsed: ['BOOTSTRAP', 'JAVASCRIPT', 'PRETTIER'],
+        liveLink: "https://gaurav-r-a-j.github.io/Api-request-app/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/Api-request-app",
+
+    },
+
+    {
+        projectNumber: "03",
+        imgUrl:  idicuss ,
+        title: "iDiscuss",
+        text: "This is my first fully backend php website in which i have added login   functionality , search functionality and many thing I learnd it from  Codewithharry yt channel",
+        techUsed: ['BOOTSTRAP', 'PHP', 'MYSQL'],
+        liveLink: "http://idiscuss.lovestoblog.com/index.php",
+        codeLink: "http://idiscuss.lovestoblog.com/index.php",
+
+    },
+
+    {
+        projectNumber: "02",
+
+        imgUrl:  portfolio1 ,
+        title: "My first Portfolio",
+        text: "I created this from a tutorial available on youtube",
+        techUsed: ['HTML', 'CSS'],
+        liveLink: "https://sstarx.in/portfolio.html",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/sstarx.github.io/blob/main/,portfolio.html"
+
+
+    },
+
+    {
+        projectNumber: "01",
+        imgUrl:  sstarx ,
+        title: "SSTARX ",
+        text: "My first Website after learning html and css and at that time I can proudly say that i have made this website with my own without watching any tutorial or else and it's my first website from where my journey starts so it's always be a special for me I named it sstarx.",
+        techUsed: ['HTML', 'CSS'],
+        liveLink: "https://sstarx.in/",
+        codeLink: "https://github1s.com/Gaurav-r-a-j/sstarx.github.io",
+    },
+
+
+]
+
+
+export default projectData
